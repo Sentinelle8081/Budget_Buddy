12cb9dddaf1a74544ce36b9abbce3b2eaaf3a28f
Sentinelle <cp.chris.trading@gmail.com>
2024-11-22 01:49:42 +0100

    aggiunto modulo per le animazioni

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from './shared/header/header.component'; // Importa HeaderComponent
 import { RouterModule } from '@angular/router';  // Importa RouterModule
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'; // Importa il modulo delle animazioni
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
-  imports: [HeaderComponent, RouterModule],  // Aggiungi HeaderComponent nella proprietà `imports`
+  imports: [HeaderComponent, RouterModule, BrowserAnimationsModule],  // Aggiungi HeaderComponent nella proprietà `imports`
 })
 export class AppComponent {
   title = 'my-app';

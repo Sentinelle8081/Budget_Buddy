ef460c27242285c868601876778c283f9ef24c30
Sentinelle <cp.chris.trading@gmail.com>
2024-11-22 03:41:37 +0100

    Aggiunto blocchi centrali con le notifiche

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,22 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from './shared/header/header.component'; // Importa HeaderComponent
 import { RouterModule } from '@angular/router';  // Importa RouterModule
-
+import { CommonModule } from '@angular/common'; // Importa il CommonModule
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
-  imports: [HeaderComponent, RouterModule],  // Aggiungi HeaderComponent nella proprietà `imports`
+  imports: [HeaderComponent, RouterModule, CommonModule],  // Aggiungi HeaderComponent nella proprietà `imports`
 })
+
 export class AppComponent {
-  title = 'my-app';
+  notifications = [
+    { message: 'Paga l’affitto entro il 25 del mese.' },
+    { message: 'Scadenza bolletta luce: 28 Novembre.' },
+  ];
+
+  // Per testare il layout vuoto, svuota l'array:
+  // notifications = [];
 }

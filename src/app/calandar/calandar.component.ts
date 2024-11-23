5ea99f72600b8804f593459e2014debf5a6329e0
Sentinelle <cp.chris.trading@gmail.com>
2024-11-23 02:11:57 +0100

    aggiunto componente calendario

---
+++ src/app/calandar/calandar.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-calandar',
+  standalone: true,
+  imports: [],
+  templateUrl: './calandar.component.html',
+  styleUrl: './calandar.component.css'
+})
+export class CalandarComponent {
+
+}
